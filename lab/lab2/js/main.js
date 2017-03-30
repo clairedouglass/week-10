c6b24696d3d32d59293d32f9a840c60e1933eecd
Claire Douglass <cldoug@upenn.edu>
2017-03-29 22:24:58 -0400

    Week 10

--- lab/lab2/js/main.js
+++ lab/lab2/js/main.js
@@ -1,8 +1,9 @@
 // Leaflet map setup
 var map = L.map('map', {
-  center: [39.923004, -75.183477],
-  zoom: 13
+  center: [40.744791, -73.979302],
+  zoom: 11
 });
+
 
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -28,8 +29,8 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolab
 // To add visualizations created with the Analysis feature, you will need to export the data to a
 // GeoJSON. From there, you can either import the GeoJSON to Carto or use Leaflet's L.geoJson.
 
-var cartoUserName = 'jfreink';
-var cartoVizId = '1f7c9af2-1088-11e7-9c60-0e05a8b3e3d7';
+var cartoUserName = 'clairedouglass';
+var cartoVizId = '621f5284-130b-11e7-b4de-0e233c30368f';
 
 var layerUrl = 'https://'+cartoUserName+'.carto.com/api/v2/viz/'+cartoVizId+'/viz.json';
 
